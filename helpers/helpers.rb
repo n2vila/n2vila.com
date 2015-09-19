49baa108fea5f24e3a7f26b821104dbccb668e7e
AJ Foster <web@aj-foster.com>
2015-09-19 02:14:37 -0400

    Add meta and open graph information, including images and twitter info.

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -1,10 +1,62 @@
 module Helpers
 
+  def meta_page_image
+    if current_page.data.image
+      locals = {
+        url: "#{site.url}#{current_page.data.image}",
+        height: current_page.data.image_height,
+        width: current_page.data.image_width,
+        type: current_page.data.image_type,
+        unique: true
+      }
+    else
+      locals = {
+        url: "#{site.url}#{site.image}",
+        height: "240",
+        width: "240",
+        type: "image/jpeg",
+        unique: (current_page.url == "/")
+      }
+    end
+
+    partial "layouts/og_image", locals: locals
+  end
+
+  def meta_page_type
+    if current_page.data.layout == "article"
+      locals = {
+        type: "article",
+        author: current_page.data.author || "AJ Foster",
+        category: current_page.data.category,
+        date: current_page.data.date,
+        tags: current_page.data.tags
+      }
+    else
+      locals = {
+        type: "website",
+        author: "AJ Foster",
+        category: nil,
+        date: nil,
+        tags: nil
+      }
+    end
+
+    partial "layouts/og_type", locals: locals
+  end
+
+  def page_description
+    escape_html(current_page.data.description || site.description)
+  end
+
   def page_title
     if current_page.data.title
-      "#{current_page.data.title} | #{site.title}"
+      escape_html("#{current_page.data.title} | #{site.title}")
     else
-      site.title
+      escape_html("#{site.title} | Web & Math")
     end
+  end
+
+  def page_url
+    "#{site.url}#{current_page.url}"
   end
 end
